bcb028a2b0a06461347634041a85336ca0f07fc0
lsy <siyu.long@qq.com>
2025-05-30 00:13:21 +0800

    chore: 删除未使用的 CSS 和 JavaScript 文件以清理项目结构

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 生成静态文件
+  images: {
+    unoptimized: true, // Cloudflare Pages 不支持 Next.js 的图片优化
+  },
 };
 
 export default nextConfig;
